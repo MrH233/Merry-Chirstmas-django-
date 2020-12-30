1a00600c307ecee9533a04cb6b61f27007bdc042
MrH233 <1658721519@qq.com>
2020-12-30 22:13:34 +0800

    django_web (better interface and logic)

--- temp1/admin.py
+++ temp1/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import letter_user
 # Register your models here.
+
+admin.site.register(letter_user)
--- temp1/views.py
+++ temp1/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-
+from django.contrib import messages
 from temp1 import models
 from django.shortcuts import HttpResponse
 import os
@@ -51,8 +51,32 @@ def letter_game(request):
     return render(request, 'temp1/letter_game.html')
 
 
+@csrf_exempt
 def letter_game_register(request):
-    return render(request, 'temp1/letter_game_register.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        phone = request.POST.get('phone')
+        qq = request.POST.get('QQ')
+        if username and password and phone and qq:
+            if models.letter_user.objects.filter(username=username):
+                messages.info(request, '用户已注册请勿重复注册!')
+                return render(request, 'temp1/letter_game_register.html')
+            elif not phone.isdigit() or not qq.isdigit():
+                messages.info(request, '请检查联系方式或QQ格式!')
+                return render(request, 'temp1/letter_game_register.html')
+            else:
+                letter_user = models.letter_user(username=username, password=password, phone=phone, QQ=qq)
+                letter_user.save()      # 保存、写入数据库
+                messages.info(request, '注册成功！')
+                return render(request, 'temp1/letter_game_register.html')
+        else:
+            messages.success(request, '请输入完整信息')
+            return render(request, 'temp1/letter_game_register.html')
+    elif request.method == 'GET':
+        return render(request, 'temp1/letter_game_register.html')
+# message 后面如果是render,则未跳转界面，可以看见提示，
+# message 后面如果是httpresponse,则跳转界面，看不见提示。
 
 
 @csrf_exempt
@@ -63,11 +87,13 @@ def letter_game_login(request):
         try:
             res = models.letter_user.objects.filter(username=name)[0]   # 返回的res对象是记录集合，而不是单个个体，但是记录不支持负索引
         except IndexError:
-            raise Http404('找不到对应用户!')
+            messages.info(request, '找不到对应用户!')
+            return render(request, 'temp1/letter_game_login.html')
         if res.password == password:
             return HttpResponseRedirect('/letter_game/play')
         else:
-            raise Http404('用户名或密码错误!')
+            messages.info(request, '用户名或密码错误!')
+            return render(request, 'temp1/letter_game_login.html')
     elif request.method == 'GET':
         return render(request, 'temp1/letter_game_login.html', {'error_msg': ''})   # 默认get方法时返回的页面
 
@@ -76,5 +102,9 @@ def letter_game_login(request):
 
 
 def letter_game_play(request):
-    return render(request, 'temp1/letter_game_play.html')
+    if request.method == 'POST':
+        hard = request.POST.get('hardlevel')
+        return render(request, 'temp1/letter_game_play.html')
+    elif request.method == 'GET':
+        return render(request, 'temp1/letter_game_play.html')
 
